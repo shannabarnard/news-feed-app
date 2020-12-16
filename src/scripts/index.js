151c962ef6c8be3cff6dec7a2fd7d2a0b0e081c4
shannabarnard <shannabarnard@gmail.com>
2020-12-16 16:45:20 +0000

    Fethcing initial data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,94 @@
 import '../styles/index.scss';
+import axios from "axios";
 
-if (process.env.NODE_ENV === 'development') {
-  require('../index.html');
+//global states
+let currentData = {};
+let filteredData = [];
+
+//fethcing initial data
+function fetchData() {
+  axios
+    .get(
+      "https://cors-anywhere.herokuapp.com/https://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion.js"
+    )
+    .then(async (res) => {
+      console.log("in");
+      //changing the format of the string
+      let splitString = res.data.split("NYTD.render_section_front(");
+      let slicedString = splitString[1].slice(0, -2);
+
+      //converting to JSON
+      currentData = JSON.parse(slicedString);
+      console.log(currentData);
+
+      //looping through the conetnt objs
+      for (
+        let contentObj = 0;
+        contentObj < currentData.page.content.length;
+        contentObj++
+      ) {
+        // console.log(contentObj);
+        //looping through the collection objs
+
+        for (
+          let collectionObj = 0;
+          collectionObj <
+          currentData.page.content[contentObj].collections.length;
+          collectionObj++
+        ) {
+          //checking if the assets length is not zero
+          if (
+            currentData.page.content[contentObj].collections[collectionObj]
+              .assets.length != 0
+          )
+            //looping through the assest objs
+            for (
+              let post = 0;
+              post <
+              currentData.page.content[contentObj].collections[collectionObj]
+                .assets.length;
+              post++
+            ) {
+              //assiging the current post
+              const currentPost =
+                currentData.page.content[contentObj].collections[collectionObj]
+                  .assets[post];
+
+              let postObj = {};
+              if (currentPost.images != undefined) {
+                console.log(currentPost.images[0]);
+
+                postObj = {
+                  headline: currentPost.headline,
+                  publishedDate: currentPost.publicationDt,
+                  summarry: currentPost.summary,
+                  url: currentPost.url,
+                  byline: currentPost.byline,
+                  image:
+                    currentPost.images.length == 0
+                      ? "https://static01.nyt.com/images/2020/12/15/world/15china-uighur1/15china-uighur1-mediumThreeByTwo210.jpg?quality=100&auto=webp"
+                      : `https://www.nytimes.com/${currentPost.images[0].types[0].content}`,
+                };
+              }
+
+              //pushing to global state
+              if (
+                postObj.headline != undefined &&
+                postObj.headline != "" &&
+                postObj.byline != undefined &&
+                postObj.publishedDate != undefined
+              )
+                filteredData.push(postObj);
+            }
+        }
+      }
+      // filteredData.map((item, key) => {});
+      console.log(filteredData);
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-console.log('webpack starterkit');
+fetchData();
